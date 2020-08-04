5231df77eec5133f47d82dee3accaf5766421d33
Jeremiah <jeremia.nadler@gmail.com>
2020-08-04 15:10:30 +0200

    added karma test runner

---
+++ karma.conf.js
@@ -0,0 +1,29 @@
+module.exports = function(config) {
+    config.set({
+      basePath: "",
+      frameworks: ["jasmine"],
+      files: [
+        { pattern: "test/**/*.test.js", watched: false  }
+      ],
+      preprocessors: {
+        './test/**/*.test.js': [ 'rollup' ]
+      },
+      rollupPreprocessor: {
+        plugins: [require('rollup-plugin-buble')()],
+        output: {
+            format: 'iife', // Helps prevent naming collisions.
+            name: 'donut-chart.js', // Required for 'iife' format.
+            sourcemap: 'inline', // Sensible for testing.
+        },
+    },
+      exclude: [],
+      reporters: ["spec"],
+      port: 9876,
+      colors: true,
+      logLevel: config.LOG_INFO,
+      autoWatch: true,
+      browsers: ["Chrome", "Firefox"],
+      singleRun: true,
+      concurrency: Infinity
+    });
+  };
